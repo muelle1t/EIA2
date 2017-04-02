2a44e3a548dbd0bdccf6d5fd1a0d686eafd8cf6f
annka <annka@DESKTOP-Q0CC7MP>
2017-04-02 17:26:58 +0200

    Aufgabe 2 Final

--- Aufgabe 2/chessboard.js
+++ Aufgabe 2/chessboard.js
@@ -1,12 +1,20 @@
+/*
+Aufgabe: Aufgabe 2a
+Name: Annkathrin Müller
+Matrikelnr.: 254868
+Datum: 02.04.2017
+
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.*/
 document.addEventListener("DOMContentLoaded", function () {
     let n = 64; //Gesamtanzahl Felder auf dem Brett
     let line = 0; // Zeilen
     let rice = 1; // Anzahl Reis
     let i = 0;
     for (i; i < n; i++) {
-        let div = document.createElement("div");
-        div.innerText = "" + rice;
-        rice = rice * 2;
+        let div = document.createElement("div"); // erstellen des Divs
+        div.innerText = "" + rice; //Anzahl Reis wird in die Div geschrieben
+        rice = rice * 2; //Anzahl Reis mal zwei, damit immer die doppelte Anzahl als auf dem Feld davor steht (Legende von Sissa Ibn Dahir)
         document.body.appendChild(div);
         if (i % 8 == 0) {
             line++;
