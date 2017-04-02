4b195b6d2ecca558bab1e9c5141329770da5e876
annka <annka@DESKTOP-Q0CC7MP>
2017-04-02 16:04:08 +0200

    Aufgabe 2 Korrektur

--- Aufgabe 2/chessboard.js
+++ Aufgabe 2/chessboard.js
@@ -1,20 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let n: number = 64; //Gesamtanzahl Felder auf dem Brett
-    let line: number = 0; // Zeilen
-    let rice: number = 1; // Anzahl Reis
-    let i: number = 0; 
-    
+document.addEventListener("DOMContentLoaded", function () {
+    let n = 64; //Gesamtanzahl Felder auf dem Brett
+    let line = 0; // Zeilen
+    let rice = 1; // Anzahl Reis
+    let i = 0;
     for (i; i < n; i++) {
-    
-        let div: HTMLDivElement = document.createElement("div");
+        let div = document.createElement("div");
         div.innerText = "" + rice;
         rice = rice * 2;
         document.body.appendChild(div);
-
         if (i % 8 == 0) {
             line++;
         }
-
         if (line % 2 == 0) {
             if (i % 2 != 0) {
                 div.style.backgroundColor = "black";
@@ -32,8 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 div.style.backgroundColor = "black";
                 div.style.color = "white";
             }
-        
+        }
     }
- }
- });
-    
+});
+//# sourceMappingURL=chessboard.js.map
