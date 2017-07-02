eabbf4fb6683b29fee18c9fed8d12ef86c83ddda
annka <annka@DESKTOP-Q0CC7MP.SpeedportEntry2090126200030>
2017-07-02 17:51:17 +0200

    Aufgabe 11

--- Aufgabe11/DatabaseClient.js
+++ Aufgabe11/DatabaseClient.js
@@ -1,12 +1,11 @@
-/*/* Aufgabe 11: Database - Suche in StudiVZ
-Name: Ina Radzuweit
-Matrikelnummer: 254672
-Datum: 30.06.2017
+/*
+Aufgabe: Aufgabe 11
+Name: Annkathrin Müller
+Matrikelnr.: 254868
+Datum: 02.07.2017
+
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
-Er wurde nicht kopiert und nicht diktiert. */ /**
-
-
-/* Dieser Code wurde in Zusammenarbeit mit Alisia Steiner bearbeitet.*/
+Er wurde nicht kopiert und auch nicht diktiert. */
 var DatabaseClient;
 (function (DatabaseClient) {
     window.addEventListener("load", init);
@@ -17,8 +16,24 @@ var DatabaseClient;
         let searchButton = document.getElementById("searchbutton");
         insertButton.addEventListener("click", insert);
         refreshButton.addEventListener("click", refresh);
-        //Event Listener für Searchbutton, der Funktion search aufruft
-        // searchButton.addEventListener("click", search); 
+        searchButton.addEventListener("click", search); //Button klickbar machen
+    }
+    // wenn der Button search geklickt wurde, wird der Name und die Matrikelnummer unter query gespeichert und ein Request versendet
+    function search(_event) {
+        let inputName = document.getElementById("searchname");
+        let inputMatrikel = document.getElementById("searchmatrikel");
+        let query = "command=search";
+        query += "&nameSearch=" + inputName.value;
+        query += "&matrikelSearch=" + inputMatrikel.value;
+        console.log(query);
+        sendRequest(query, handleSearchResponse);
+    }
+    // wenn eine Response gekommen ist, wie sie verarbeitet/wie die Ausgabe aussehen soll
+    function handleSearchResponse(_event) {
+        let xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
     }
     function insert(_event) {
         let inputs = document.getElementsByTagName("input");
@@ -35,9 +50,8 @@ var DatabaseClient;
     }
     function sendRequest(_query, _callback) {
         let xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?" + _query, true);
-        //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
-        //readystatechange = Event, dass Status sich geändert hat, _callback ist Parameter
+        //        xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "https://senddata11.herokuapp.com/" + _query, true);
         xhr.addEventListener("readystatechange", _callback);
         xhr.send();
     }
@@ -52,8 +66,8 @@ var DatabaseClient;
         if (xhr.readyState == XMLHttpRequest.DONE) {
             let output = document.getElementsByTagName("textarea")[0];
             output.value = xhr.response;
-            let responseAsJson = JSON.parse(xhr.response);
-            console.log(responseAsJson);
+            //            let responseAsJson: JSON = JSON.parse(xhr.response);
+            console.log(xhr.response);
         }
     }
 })(DatabaseClient || (DatabaseClient = {}));
--- Aufgabe11/DatabaseClient.ts
+++ Aufgabe11/DatabaseClient.ts
@@ -10,6 +10,8 @@ Er wurde nicht kopiert und auch nicht diktiert. */
 namespace DatabaseClient {
     window.addEventListener("load", init);
 
+    
+
     function init(_event: Event): void {
         console.log("Init");
         let insertButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("insert");
@@ -17,9 +19,64 @@ namespace DatabaseClient {
         let searchButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("searchbutton");
         insertButton.addEventListener("click", insert);
         refreshButton.addEventListener("click", refresh);
-        
-        searchButton.addEventListener("click", searchMatrikel); 
+        searchButton.addEventListener("click", search); //Button klickbar machen
+    }
+    // wenn der Button search geklickt wurde, wird der Name und die Matrikelnummer unter query gespeichert und ein Request versendet
+    function search(_event: Event): void {
+        let inputName: HTMLInputElement = <HTMLInputElement>document.getElementById("searchname");
+        let inputMatrikel: HTMLInputElement = <HTMLInputElement>document.getElementById("searchmatrikel");
+        let query: string = "command=search";
+        query += "&nameSearch=" + inputName.value;
+        query += "&matrikelSearch=" + inputMatrikel.value;
+        console.log(query);
+        sendRequest(query, handleSearchResponse);
+    }
+    // wenn eine Response gekommen ist, wie sie verarbeitet/wie die Ausgabe aussehen soll
+    function handleSearchResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
     }
 
+    function insert(_event: Event): void {
+        let inputs: NodeListOf<HTMLInputElement> = document.getElementsByTagName("input");
+        let query: string = "command=insert";
+        query += "&name=" + inputs[0].value;
+        query += "&firstname=" + inputs[1].value;
+        query += "&matrikel=" + inputs[2].value;
+        console.log(query);
+        sendRequest(query, handleInsertResponse);
     }
-   
+
+    function refresh(_event: Event): void {
+        let query: string = "command=find";
+        sendRequest(query, handleFindResponse);
+    }
+
+    function sendRequest(_query: string, _callback: EventListener): void {
+        let xhr: XMLHttpRequest = new XMLHttpRequest();
+//        xhr.open("GET", "http://localhost:8100?" + _query, true);
+        xhr.open("GET", "https://senddata11.herokuapp.com/" + _query, true);
+        xhr.addEventListener("readystatechange", _callback);
+        xhr.send();
+    }
+
+    function handleInsertResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+
+    function handleFindResponse(_event: ProgressEvent): void {
+        let xhr: XMLHttpRequest = (<XMLHttpRequest>_event.target);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            let output: HTMLTextAreaElement = document.getElementsByTagName("textarea")[0];
+            output.value = xhr.response;
+//            let responseAsJson: JSON = JSON.parse(xhr.response);
+            console.log(xhr.response);
+        }
+    }
+}
+    
