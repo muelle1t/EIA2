3eaf52861b75f961b7420441e6dff5bbb65466bd
annka <annka@LAPTOP-G9S3AKR1.SpeedportEntry2090126250021>
2018-06-10 16:07:26 +0200

    Update

---
+++ Aufgabe8/Interfaces.js
@@ -0,0 +1,6 @@
+var Aufgabe08_Interfaces;
+(function (Aufgabe08_Interfaces) {
+    // Homogenes assoziatives Array zur Speicherung eines Studenten unter der Matrikelnummer
+    Aufgabe08_Interfaces.studiHomoAssoc = {};
+})(Aufgabe08_Interfaces || (Aufgabe08_Interfaces = {}));
+//# sourceMappingURL=Interfaces.js.map
